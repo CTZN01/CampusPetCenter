5b474fa266583133d861d9eb1135547aca304943
ctzn <1786879132@qq.com>
2025-12-13 00:04:25 +0800

    first commit

---
+++ src/main/java/com/ashen/campuspetcenter/CampusPetCenterApplication.java
@@ -0,0 +1,13 @@
+package com.ashen.campuspetcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusPetCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusPetCenterApplication.class, args);
+    }
+
+}
